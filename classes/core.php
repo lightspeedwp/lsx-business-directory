b2cb216a6cd3d0ec1c0a73673596fd0c8913d0ef
Warwick <warwick@feedmymedia.com>
2016-03-23 23:07:00 +0200

    Added in the gulp file to generate the css, and the JS

--- classes/core.php
+++ classes/core.php
@@ -118,8 +118,8 @@ class LSX_Business_Directory extends Lsx {
 	 */
 	public function enqueue_scripts() {
 		
-		//wp_enqueue_style('lsx_landing_pages_style', LSX_BUSINESS_DIRECTORY_URL.'/assets/css/style.css');
-		wp_enqueue_script('lsx_business_directory_script', LSX_BUSINESS_DIRECTORY_URL . 'assets/js/lsx-business-directory.js', array('jquery'), null, false);
+		wp_enqueue_style('lsx_business_directory_style', LSX_BUSINESS_DIRECTORY_URL.'assets/css/frontend.css');
+		wp_enqueue_script('lsx_business_directory_script', LSX_BUSINESS_DIRECTORY_URL . 'assets/js/frontend.min.js', array('jquery'), null, false);
 		
 		//Set some parameters that we can use in the JS
 		/*$is_portfolio = false;
