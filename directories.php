ddc71fca43021287c633777a7c9db1911e434040
Warwick <warwick@feedmymedia.com>
2016-06-07 12:19:30 +0200

    no message

--- directories.php
+++ directories.php
@@ -10,7 +10,7 @@
  * Plugin Name: LSX Business Directory
  * Plugin URI:  http://lsdev.biz/
  * Description: Business Directory addon for the LSX Framework
- * Version:     1.0.0
+ * Version:     1.0.1
  * Author:      LightSpeed Team
  * Author URI:  https://www.lsdev.biz/
  * Text Domain: lsx-business-directory
