cfcbb06fc4c3e1d997b4ba69a5d8ab86d65c2bcf
Warwick Booth <warwick@lsdev.biz>
2020-03-11 12:45:55 +0200

    Replacing the old archive template with the starter plugin template

--- templates/archive-business-directory.php
+++ templates/archive-business-directory.php
@@ -6,152 +6,50 @@
  * @subpackage  template
  * @category    archive
  */
-?>
-<?php get_header(); ?>
+get_header(); ?>
 
-	<div id="primary" class="content-area <?php echo esc_attr( lsx_main_class() ); ?>">
+<?php lsx_content_wrap_before(); ?>
 
-		<?php lsx_content_before(); ?>
+<div id="primary" class="content-area <?php echo esc_attr( lsx_main_class() ); ?>">
 
-		<main id="main" class="site-main" role="main">
+	<?php lsx_content_before(); ?>
 
-			<?php lsx_content_top(); ?>
+	<main id="main" class="site-main" role="main">
 
-			<header class="page-header tours-archive-header">
-				<?php if ( is_post_type_archive() ) { ?>
-					<h1 class="page-title"><?php esc_html_e( 'Business Directory', 'lsx-business-directory' ); ?></h1>
-				<?php } else { ?>
-					<h1 class="page-title">
-					<?php
-					if ( is_tax() ) {
-						$taxo          = get_taxonomy( get_queried_object()->taxonomy );
-						$post_type     = $taxo->object_type;
-						$post_type_obj = get_post_type_object( $post_type[0] );
-						echo esc_attr( $post_type_obj->labels->name ) . ': ' . single_term_title( '', false );
-					} else {
-						the_archive_title();
-					}
+		<?php lsx_content_top(); ?>
+
+		<?php if ( have_posts() ) : ?>
+
+			<div class="post-wrapper">
+				<?php
+				while ( have_posts() ) :
+					the_post();
 					?>
-					</h1>
-				<?php } ?>
+					<?php include( LSX_BD_PATH . '/templates/single-row-business.php' ); ?>
+					<?php
+				endwhile;
+				?>
+			</div>
 
-			</header><!-- .entry-header -->
+			<?php lsx_paging_nav(); ?>
 
+		<?php else : ?>
 
-			<?php if ( is_tax() ) { ?>
-				<div class="entry-content">
-					<?php the_archive_description(); ?>
-				</div>
-			<?php } ?>
+			<?php get_template_part( 'partials/content', 'none' ); ?>
 
+		<?php endif; ?>
 
-					<?php if ( have_posts() ) : ?>
-						<?php
-						$lsx_search_active = false; // TODO: detect if lsx search is on
-						$lsx_layout        = 'row'; // TODO: row or col, get this settings somewhere
-						$layout_classes    = 'facetwp-template'; // default one for list
+		<?php lsx_content_bottom(); ?>
 
-						if ( 'col' === $lsx_layout ) {
-							$layout_classes = 'facetwp-template lsx-grid';
-						}
+	</main><!-- #main -->
 
-						if ( $lsx_search_active ) {
-							$filter_classes     = 'col-md-3';
-							$search_classes     = 'business-filters';
-							$pagination_classes = '';
-							$results_classes    = 'col-md-9';
-						} else {
-							$filter_classes     = 'lsx-hide';
-							$search_classes     = 'lsx-hide';
-							$pagination_classes = 'lsx-hide';
-							$results_classes    = 'col-md-12';
-						}
+	<?php lsx_content_after(); ?>
 
-						?>
-						<div class="lsx-business-directory-wrapper">
-							<div class="row">
+</div><!-- #primary -->
 
-								<div class="<?php print esc_attr( $filter_classes ); ?>">
-									<div class="business-facets">
-										<h3><?php esc_html_e( 'Refine the Results', 'lsx-business-directory' ); ?></h3>
-										<h4><?php esc_html_e( 'Keyword Search', 'lsx-business-directory' ); ?></h4>
-										<?php echo do_shortcode( '[facetwp facet="post_search"]' ); ?>
-										<h4><?php esc_html_e( 'Industry', 'lsx-business-directory' ); ?></h4>
-										<?php echo do_shortcode( '[facetwp facet="industries"]' ); ?>
-										<h4><?php esc_html_e( 'Region', 'lsx-business-directory' ); ?></h4>
-										<?php echo do_shortcode( '[facetwp facet="regions"]' ); ?>
-									</div>
-								</div>
+<?php lsx_content_wrap_after(); ?>
 
-								<div class="<?php print esc_attr( $results_classes ); ?>">
+<?php get_sidebar(); ?>
 
-									<div class="business-listings">
-
-										<div class="<?php print esc_attr( $search_classes ); ?>">
-											<div class="business-filters-top">
-												<?php echo do_shortcode( '[facetwp sort="true"]' ); ?>
-												<?php echo do_shortcode( '[facetwp per_page="true"]' ); ?>
-											</div>
-
-											<div class="business-filters-bottom">
-												<?php echo do_shortcode( '[facetwp facet="alphabet"]' ); ?>
-												<?php echo do_shortcode( '[facetwp pager="true"]' ); ?>
-											</div>
-										</div>
-										<div class="<?php print esc_attr( $layout_classes ); ?>">
-										<?php
-										while ( have_posts() ) :
-											the_post();
-
-											if ( 'col' === $lsx_layout ) {
-												lsx_business_col();
-											} else {
-												lsx_business_row();
-											}
-											?>
-										<?php endwhile; ?>
-										</div>
-										<div class="<?php print esc_attr( $pagination_classes ); ?>"><?php echo do_shortcode( '[facetwp pager="true"]' ); ?></div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-					<?php else : ?>
-
-						<section class="no-results not-found">
-							<header class="page-header">
-								<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'lsx-business-directory' ); ?></h1>
-							</header><!-- .page-header -->
-
-							<div class="page-content">
-								<?php if ( current_user_can( 'publish_posts' ) ) : ?>
-
-									<p>
-										<?php
-										// translators: %1$s is replaced with a link.
-										printf( esc_html__( 'Ready to publish your first tour? <a href="%1$s">Get started here</a>.', 'lsx-business-directory' ), esc_url( admin_url( 'post-new.php?post_type=business-directory' ) ) );
-										?>
-									</p>
-
-								<?php else : ?>
-
-									<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'lsx-business-directory' ); ?></p>
-									<?php get_search_form(); ?>
-
-								<?php endif; ?>
-							</div><!-- .page-content -->
-						</section><!-- .no-results -->
-
-					<?php endif; ?>
-
-
-			<div class="clearfix"></div>
-
-		</main><!-- #main -->
-
-		<?php // lsx_content_after(); ?>
-
-	</div><!-- #primary -->
-
-<?php get_footer(); ?>
+<?php
+get_footer();
