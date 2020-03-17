8a0a38e97d44586d474dd03e7bb62dd4b75ce721
Warwick Booth <warwick@lsdev.biz>
2020-03-17 18:14:56 +0200

    More Related Listings fixes.

--- templates/single-col-business.php
+++ templates/single-col-business.php
@@ -42,19 +42,19 @@
 
 					<?php if ( $location ) : ?>
 					<div class="location">
-						<span><i class="fa fa-globe-africa"></i><strong><?php esc_html_e( 'Location', 'lsx-business-directory' ); ?>: </strong><?php echo esc_attr( $location ); ?></span>
+						<span><i class="fa fa-globe-africa"></i> <strong><?php esc_html_e( 'Location', 'lsx-business-directory' ); ?>: </strong><?php echo esc_attr( $location ); ?></span>
 					</div>
 					<?php endif; ?>
 
 					<?php if ( $primary_phone ) : ?>
 					<div class="telephone">
-						<span><i class="fa fa-phone-square-alt"></i><strong><?php esc_html_e( 'Phone', 'lsx-business-directory' ); ?>: </strong> <a href="tel:<?php echo esc_attr( str_replace( ' ', '', $primary_phone ) ); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_attr( $primary_phone ); ?></a></span>
+						<span><i class="fa fa-phone-square"></i> <strong><?php esc_html_e( 'Phone', 'lsx-business-directory' ); ?>: </strong> <a href="tel:<?php echo esc_attr( str_replace( ' ', '', $primary_phone ) ); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_attr( $primary_phone ); ?></a></span>
 					</div>
 					<?php endif; ?>
 
 					<?php if ( $primary_email ) : ?>
 					<div class="email">
-						<span><i class="fa fa-envelope-square"></i><strong><?php esc_html_e( 'Email', 'lsx-business-directory' ); ?>: </strong> <a href="mailto:<?php echo esc_attr( $primary_email ); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_attr( $primary_email ); ?></a></span>
+						<span><i class="fa fa-envelope-square"></i> <strong><?php esc_html_e( 'Email', 'lsx-business-directory' ); ?>: </strong> <a href="mailto:<?php echo esc_attr( $primary_email ); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_attr( $primary_email ); ?></a></span>
 					</div>
 					<?php endif; ?>
 				</div>
