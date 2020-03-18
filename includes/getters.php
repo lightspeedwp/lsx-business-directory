ac9612d1df5a3ad8162a736689a8836ed0a323f1
Warwick Booth <warwick@lsdev.biz>
2020-03-18 15:06:50 +0200

    Reducing the labels of the placeholders to fix th e css.

--- includes/getters.php
+++ includes/getters.php
@@ -85,14 +85,14 @@ function get_placeholder_fields( $prefix = '' ) {
 	$fields = array(
 		array(
 			'name'         => esc_html__( 'Banner Placeholder', 'lsx-business-directory' ),
-			'desc'         => esc_html__( 'This placeholder will display if no set image is found. Leaving it blank will default to a banner colour. Your image should be 1920px x 600px preferably, but no less than 1440px x 430px.', 'lsx-business-directory' ),
+			'desc'         => esc_html__( 'Your image should be 1920px x 600px preferably, but no less than 1440px x 430px.', 'lsx-business-directory' ),
 			'id'           => '_banner_placeholder',
 			'type'         => 'file',
 			'preview_size' => 'lsx-thumbnail-wide',
 		),
 		array(
 			'name'         => esc_html__( 'Featured Placeholder', 'lsx-business-directory' ),
-			'desc'         => esc_html__( 'This placeholder will display if no set featured image is set. Your image should be 800px x 600px preferably, but no less than 360px x 168px.', 'lsx-business-directory' ),
+			'desc'         => esc_html__( 'Your image should be 800px x 600px preferably, but no less than 360px x 168px.', 'lsx-business-directory' ),
 			'id'           => '_thumbnail_placeholder',
 			'type'         => 'file',
 			'preview_size' => 'lsx-thumbnail-wide',
