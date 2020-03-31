fe7acca167e5380919ec8016d4779aa33241c015
Warwick Booth <warwick@lsdev.biz>
2020-03-31 16:51:50 +0200

    Sorting out the admin JS

---
+++ assets/js/src/lsx-bd-admin.js
@@ -0,0 +1,15 @@
+/**
+ * LSX Starter Plugin scripts (admin).
+ *
+ * @package lsx-business-directory
+ */
+
+var LSX_BD_ADMIN = Object.create( null );
+
+;( function( $, window, document, undefined ) {
+
+	'use strict';
+
+    LSX_BD_ADMIN.document = $( document );
+
+} )( jQuery, window, document );
