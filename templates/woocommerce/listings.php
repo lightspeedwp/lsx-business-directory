14880d912b0c862b992813e950d1d944277175b6
Warwick Booth <warwick@lsdev.biz>
2020-04-06 20:18:28 +0200

    Added in the no listing view.

--- templates/woocommerce/listings.php
+++ templates/woocommerce/listings.php
@@ -1,3 +1,13 @@
-<div class="business-row-wrapper col-xs-12 col-sm-12 col-md-12">
-
+<div class="lsx-bd-my-account listings">
+	<?php
+	if ( is_wc_endpoint_url( 'listings' ) ) {
+		?>
+		<div class="no-listings">
+			<i class="fa fa-list"></i>
+			<h3><?php echo esc_attr( 'You have not added any listings yet,', 'lsx-business-directory' ); ?><br /><?php echo esc_attr( 'click here to add your business', 'lsx-business-directory' ); ?></h3>
+			<a class="btn btn-secondary" href=""><?php echo esc_attr( 'Add new listing', 'lsx-business-directory' ); ?> <i class="fa fa-plus-circle"></i></a>
+		</div>
+		<?php
+	}
+	?>
 </div>
