3dc0a8c01a9f3d456e6bfab8c2ec3452434db319
Warwick Booth <warwick@lsdev.biz>
2020-04-06 19:10:56 +0200

    Fixed the redirect to the my listings tab

--- classes/integrations/class-woocommerce.php
+++ classes/integrations/class-woocommerce.php
@@ -21,6 +21,7 @@ class Woocommerce {
 	 * Contructor
 	 */
 	public function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
 	}
 
 	/**
@@ -36,5 +37,78 @@ class Woocommerce {
 			self::$instance = new self();
 		}
 		return self::$instance;
+	}
+
+	/**
+	 * Initiator
+	 */
+	public function init() {
+		if ( function_exists( 'WC' ) ) {
+			$this->init_query_vars();
+			add_filter( 'woocommerce_get_query_vars', array( $this, 'add_query_vars' ) );
+			add_filter( 'woocommerce_account_menu_items', array( $this, 'register_my_account_tabs' ) );
+			add_action( 'woocommerce_account_my-listings_endpoint', array( $this, 'endpoint_content' ) );
+		}
+	}
+
+	/**
+	 * Init query vars by loading options.
+	 *
+	 * @since 2.0
+	 */
+	public function init_query_vars() {
+		$this->query_vars = array(
+			'my-listings' => $this->get_my_listings_endpoint(),
+		);
+	}
+
+	/**
+	 * Hooks into `woocommerce_get_query_vars` to make sure query vars defined in
+	 * this class are also considered `WC_Query` query vars.
+	 *
+	 * @param  array $query_vars
+	 * @return array
+	 */
+	public function add_query_vars( $query_vars ) {
+		return array_merge( $query_vars, $this->query_vars );
+	}
+
+	/**
+	 * Reset the woocommerce_myaccount_view_subscriptions_endpoint option name to woocommerce_myaccount_view_subscription_endpoint
+	 *
+	 * @return mixed Value set for the option
+	 * @since 2.2.18
+	 */
+	private function get_my_listings_endpoint() {
+		$value = lsx_bd_get_option( 'translations_my_listings_endpoint', false );
+		if ( false !== $value && '' !== $value ) {
+			$endpoint = $value;
+		} else {
+			$endpoint = 'my-listings';
+		}
+		return $endpoint;
+	}
+
+	/**
+	 * Registers the My Listing My account tab
+	 *
+	 * @param  array $menu_links
+	 * @return void
+	 */
+	public function register_my_account_tabs( $menu_links ) {
+		$new_links = array(
+			'my-listings' => __( 'My Listings', 'lsx-business-directory' ),
+		);
+		$menu_links = array_slice( $menu_links, 0, 1, true ) + $new_links + array_slice( $menu_links, 1, null, true );
+		return $menu_links;
+	}
+
+	/**
+	 * Gets the endpoint content
+	 *
+	 * @return void
+	 */
+	public function endpoint_content() {
+		
 	}
 }
