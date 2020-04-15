3c88e2c3c505ad7d9993d8aa552bac7e983f7eb9
Warwick Booth <warwick@lsdev.biz>
2020-04-15 16:25:58 +0200

    Added in the function to set the subscription as a listing subscription.

--- classes/integrations/woocommerce/class-checkout.php
+++ classes/integrations/woocommerce/class-checkout.php
@@ -33,6 +33,7 @@ class Checkout {
 		if ( 'on' === lsx_bd_get_option( 'woocommerce_enable_checkout', false ) ) {
 			add_filter( 'woocommerce_add_to_cart_validation', array( $this, 'maybe_clear_cart' ), 20, 6 );
 			add_action( 'woocommerce_checkout_order_processed', array( $this, 'mark_order_as_listing' ), 20, 3 );
+			add_action( 'woocommerce_checkout_create_subscription', array( $this, 'mark_subscription_as_listing' ), 20, 4 );
 		}
 	}
 
@@ -83,6 +84,7 @@ class Checkout {
 	 */
 	public function mark_order_as_listing( $order_id, $posted_data, $order ) {
 		$order_items = $order->get_items();
+		$listing_set = false;
 		if ( ! empty( $order_items ) ) {
 			foreach ( $order_items as $item_id => $item ) {
 				$product_id = $item->get_product_id();
@@ -90,9 +92,27 @@ class Checkout {
 					$is_listing = get_post_meta( $product_id, '_lsx_bd_listing', true );
 					if ( 'yes' === $is_listing ) {
 						add_post_meta( $order_id, '_lsx_bd_listing', 'yes', true );
+						$listing_set = true;
 					}
 				}
 			}
+		}
+		return $listing_set;
+	}
+
+	/**
+	 * Undocumented function
+	 *
+	 * @param [type] $subscription
+	 * @param [type] $posted_data
+	 * @param [type] $order
+	 * @param [type] $cart
+	 * @return void
+	 */
+	public function mark_subscription_as_listing( $subscription, $posted_data, $order, $cart ) {
+		$is_listing_order = $this->mark_order_as_listing( $order->get_id(), $posted_data, $order );
+		if ( true === $is_listing_order ) {
+			add_post_meta( $subscription->get_id(), '_lsx_bd_listing', 'yes', true );
 		}
 	}
 }
