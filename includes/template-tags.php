7dc4e80bd3f0b614bab18229af75bd2ee78bc1fd
Warwick Booth <warwick@lsdev.biz>
2020-05-18 17:52:54 +0200

    Fixing the translation of the listings button.

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -535,7 +535,7 @@ function lsx_bd_listing_map( $before = '', $after = '', $echo = true ) {
  */
 function lsx_bd_add_listing_button() {
 	?>
-		<a class="btn btn-secondary" href="<?php echo esc_attr( wc_get_page_permalink( 'myaccount' ) . lsx_bd_get_option( 'translations_listings_add_endpoint', 'add-listing' ) ); ?>/"><?php echo esc_attr__( 'Add new listing', 'lsx-business-directory' ); ?> <i class="fa fa-plus-circle"></i></a>
+		<a class="btn btn-secondary" href="<?php echo esc_attr( get_permalink( wc_get_page_id( 'myaccount' ) ) . lsx_bd_get_option( 'translations_listings_add_endpoint', 'add-listing' ) ); ?>/"><?php echo esc_attr__( 'Add new listing', 'lsx-business-directory' ); ?> <i class="fa fa-plus-circle"></i></a>
 	<?php
 }
 
@@ -547,7 +547,7 @@ function lsx_bd_add_listing_button() {
  */
 function lsx_bd_edit_listing_button() {
 	?>
-		<a href="<?php echo esc_attr( wc_get_page_permalink( 'myaccount' ) . lsx_bd_get_option( 'translations_listings_edit_endpoint', 'edit-listing' ) ); ?>/<?php the_ID(); ?>/" class="btn tertiary-border-btn"><?php echo esc_attr__( 'Edit', 'lsx-business-directory' ); ?></a>
+		<a href="<?php echo esc_attr( get_permalink( wc_get_page_id( 'myaccount' ) ) . lsx_bd_get_option( 'translations_listings_edit_endpoint', 'edit-listing' ) ); ?>/<?php the_ID(); ?>/" class="btn tertiary-border-btn"><?php echo esc_attr__( 'Edit', 'lsx-business-directory' ); ?></a>
 	<?php
 }
 
